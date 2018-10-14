34c91de47b77b439bf8c9812ccd6016b6250235f
TheAfroDucc <44143053+TheAfroDucc@users.noreply.github.com>
2018-10-14 15:17:09 -0400

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+
+package duoworkaround.autoapprover;
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.grantPermission);
+
+        //Open android settings to set notififcation view permissions
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+                startActivity(intent);
+            }
+        });
+    }
+}
